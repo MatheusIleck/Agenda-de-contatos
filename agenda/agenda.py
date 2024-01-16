3da53061370f2b2f3d6dc328feb34c2f83ab0b5d
Matheus Ileck Farias <matheus.ileck556@gmail.com>
2024-01-16 19:24:09 -0300

    Atualizando editar contatos

--- agenda/agenda.py
+++ agenda/agenda.py
@@ -25,8 +25,10 @@ class Agenda():
             lista = pickle.load(arquivo)
         if len(lista) > 0:
            self.contatos = lista
+           return self.contatos
         else:
             self.contatos = []
+            return self.contatos
 
     def salvar_contatos(self, contatos=None):
         if contatos == None:
@@ -36,7 +38,6 @@ class Agenda():
             self.contatos.extend(contatos)
             with open("agenda.pkl", 'wb') as arquivo:
                     pickle.dump(self.contatos, arquivo)
-        
 
     def exibir_contatos(self):
         cabecalho('AGENDA')
@@ -45,34 +46,15 @@ class Agenda():
         if len(lista) > 0:
             for i, contato in enumerate(lista):
                 print(f'\033[1;32m{i}- Nome:\033[m {contato["nome"]:<30} \033[1;32mNumero: \033[m{contato["numero"]:>3}')
-                i+= 1
+                i+= 1  
         else:
-            print(f'\033[;31mVocê não tem contatos.\033[m')
-            
-            
-    def editar_contatos(self):
-            try:
-                if len(self.contatos) <= 0:
-                    pass
-                else:
-                    linha()
-                    edita_contato = int(input('Qual contato você deseja editar?'))
-                    while True:
-                        if edita_contato > len(self.contatos) - 1:
-                            edita_contato = int(input(f'\033[;31mPor favor Digite um valor valido: \033[m'))
-                        else:
-                            nome = sanitizar_nome('Digite o Nome: ')
-                            numero = sanitizar_numero('Digite o Número: ')
-                            self.contatos[edita_contato]["nome"] = nome
-                            self.contatos[edita_contato]["numero"] = numero
-                            print(f'\033[;32mContato Atualizado\033[m')
-                            break
-                       
-            except (ValueError):
-                print(f'\033[;31mO usuario digitou um valor invalido\033[m')
-            except KeyboardInterrupt:
-                print(f'\033[;32mSaindo...\033[m')
-
+             print(f'\033[;31mVocê não tem contatos.\033[m')
+             
+    def editar_contatos(self, contato_escolhido, nome, numero):
+            self.contatos[contato_escolhido]["nome"] = nome
+            self.contatos[contato_escolhido]["numero"] = numero
+            print(f'\033[;32mContato Atualizado\033[m')
+                           
     def remover_contatos(self):
         if len(self.contatos) <= 0:
                     pass
@@ -87,4 +69,11 @@ class Agenda():
                     print(f'\033[;32mContato removido!\033[m')
                     self.contatos.pop(remove_contato)
                     break
+    
+    def encontrar_contato(self, numero):
+        contato_escolhido = self.contatos[numero]
+        print(contato_escolhido)
    
+    def pegar_contatos(self):
+        return self.contatos
+
--- interface/interface.py
+++ interface/interface.py
@@ -7,7 +7,7 @@ agenda = Agenda()
 class interface():
     def __init__(self, agenda, Contato):
         agenda.criar_arquivo()
-        agenda.verificar_agenda()
+        lista = agenda.verificar_agenda()
 
 
     def mostrar_comandos():
@@ -20,12 +20,26 @@ class interface():
             
             
         if resposta == 2:
+            esperando_por_input = False
             try:
                 agenda.exibir_contatos()
-                agenda.editar_contatos()
-                agenda.salvar_contatos()
+                contatos = agenda.pegar_contatos()
+                if len(contatos) > 0:
+                    esperando_por_input = True
+                while esperando_por_input == True:
+                    funções.linha()
+                    index_contato = int(input('Qual contato você deseja editar?'))
+                    contato_escolhido = agenda.encontrar_contato()
+                    if contato_escolhido in contatos:
+                        nome = funções.sanitizar_nome('Digite o Nome: ')
+                        numero = funções.sanitizar_numero('Digite o Numero: ')
+                        agenda.editar_contatos(index_contato, nome, numero)
+                        agenda.salvar_contatos()
+                        esperando_por_input = False
             except(KeyboardInterrupt):
                 print(f'\033[32mSaindo...\033[m')
+            except (ValueError,TypeError):
+                print(f'\033[;31mErro: O usuario informou um dado inválido\033[m')
             
         if resposta == 4:
             agenda.exibir_contatos()
